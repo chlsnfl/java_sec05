2f14032d2efed61f29ea5ca79ee295fee41ccf7f
chlsnfl <cnr1108@naver.com>
2023-03-28 17:58:19 +0900

    getter, setter

---
+++ sec05/src/ch14/Member/MemberInfo.java
@@ -0,0 +1,37 @@
+package ch14.Member;
+
+public class MemberInfo {
+	
+	private String userid;
+	private String password;
+	private String username;
+	private String adress;
+	
+	
+	public String getUserid() {
+		return userid;
+	}
+	public void setUserid(String userid) {
+		this.userid = userid;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getAdress() {
+		return adress;
+	}
+	public void setAdress(String adress) {
+		this.adress = adress;
+	}
+	
+	
+}
---
+++ sec05/src/sec06/ch12/Application.java
@@ -0,0 +1,9 @@
+package sec06.ch12;
+
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
